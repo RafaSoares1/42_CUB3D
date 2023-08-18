cb3fa0eebdc8abeb528c0a37665e219982596d69
Rafael Soares <103336451+RafaSoares1@users.noreply.github.com>
2023-08-18 17:15:39 +0100

    Add files via upload

---
+++ mandatory/cub3d.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emsoares <emsoares@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 14:12:07 by emsoares          #+#    #+#             */
+/*   Updated: 2023/08/18 15:28:17 by emsoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libft/libft.h"
+# include "../minilibx-linux/mlx.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <X11/X.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		x;
+	int		y;
+	int		fd;
+	char	**map;
+}				t_data;
+
+
+//****************utils.c********************
+void	ft_init_stack(t_data *d);
+
+#endif
