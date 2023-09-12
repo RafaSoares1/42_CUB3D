b7e8e688b9a868eb53b4b4d6c9670691ff4676ed
Jose Pedro Miranda Goncalves <jomirand@c1r11s11.42porto.com>
2023-09-12 12:16:47 +0100

    player

--- mandatory/operations/game.c
+++ mandatory/operations/game.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   game.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emsoares <emsoares@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: jomirand <jomirand@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 16:44:34 by jomirand          #+#    #+#             */
-/*   Updated: 2023/09/12 11:28:50 by emsoares         ###   ########.fr       */
+/*   Updated: 2023/09/12 12:13:20 by jomirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,35 @@ void	start_game(t_data *d)
 	mlx_loop(d->mlx_ptr);
 }
 
-void draw_square(int x, int y, int size, t_data *d)
+void draw_grid(int x, int y, int color, t_data *d)
 {
-  int i;
-  int j;
+	int	i;
+	int	j;
 
 	i = x;
-  while(i < x + size)
+	while(i < x + SIZE)
 	{
-    mlx_pixel_put(d->mlx_ptr,d->win_ptr, i, y, 0x00FF00);			
+		mlx_pixel_put(d->mlx_ptr,d->win_ptr, i, y, color);
 		i++;
-  }
+	}
 	i = x;
-	while(i < x + size)
+	while(i < x + SIZE)
 	{
-    mlx_pixel_put(d->mlx_ptr,d->win_ptr, i, y + size - 1, 0x00FF00);			
+		mlx_pixel_put(d->mlx_ptr,d->win_ptr, i, y + SIZE - 1, color);
 		i++;
-  }
+	}
 	j = y;
-	while(j < y + size)
+	while(j < y + SIZE)
 	{
-    mlx_pixel_put(d->mlx_ptr,d->win_ptr, x, j, 0x00FF00);			
+		mlx_pixel_put(d->mlx_ptr,d->win_ptr, x, j, color);
 		j++;
-  }
+	}
 	j = y;
-	while(j < y + size)
+	while(j < y + SIZE)
 	{
-    mlx_pixel_put(d->mlx_ptr,d->win_ptr, x + size - 1, j, 0x00FF00);			
+		mlx_pixel_put(d->mlx_ptr,d->win_ptr, x + SIZE - 1, j, color);
 		j++;
-  }
+	}
 }
 
 void	draw_minimap(t_data *d)
@@ -58,119 +58,64 @@ void	draw_minimap(t_data *d)
 	int y = 0;
 	int	x = 0;
 
-  while (y < d->count_lines)
+	while (y < d->count_lines)
 	{
 		x = 0;
-    while (d->map_utils->map[y][x])
+		while (d->map_utils->map[y][x])
 		{
-      if (d->map_utils->map[y][x] == '1')
-        draw_square(x * 50, y * 50, 50, d);
-			x++;
-    }
-		y++;
-  }
-}
-
-/* void	draw_minimap(t_data *d)
-{
-	int	minimap_width;
-	int	minimap_height;
-	int	i;
-	int	j;
-	int	x;
-	int	y;
-
-	minimap_width = WIDTH / 500;
-	minimap_height = HEIGHT / 100;
-	i = 0;
-	x = 0;
-	y = 0;
-
-	while(y < d->count_lines)
-	{
-		x = 0;
-		while(x < d->line_length)
-		{
-			i = 0;
-			j = (minimap_width * x) + 1;
-			if(d->map_utils->map[y][x] == '1')
+			if (d->map_utils->map[y][x] == '1')
 			{
-				while(i < minimap_height)
-				{
-					if (j == minimap_width)
-						j = 0;
-					while(j <= minimap_width * 3)
-					{
-						mlx_pixel_put(d->mlx_ptr, d->win_ptr, j, i, 0xFF00);
-						j++;
-					}
-					i++;
-				}
+				draw_square(x * 50, y * 50, WALL, d);
+				draw_grid(x * 50, y * 50, GRID, d);
 			}
 			if (d->map_utils->map[y][x] == '0')
 			{
-				while(i < minimap_height)
-				{
-					while(j < minimap_width)
-					{
-						mlx_pixel_put(d->mlx_ptr, d->win_ptr, j, i, 0xFF0000);
-						j++;
-					}
-					i++;
-				}
+				draw_square(x * 50, y * 50, FLOOR, d);
+				draw_grid(x * 50, y * 50, GRID, d);
 			}
-			if (d->map_utils->map[y][x] == 'N')
+			if (d->map_utils->map[y][x] == 'N') //falta S, E e W
 			{
-				while(i < 10)
-				{
-					while(j < 10)
-					{
-						mlx_pixel_put(d->mlx_ptr, d->win_ptr, j, i, 0x005dff);
-						j++;
-					}
-					i++;
-				}
+				draw_square(x * 50, y * 50, FLOOR, d);
+				draw_grid(x * 50, y * 50, GRID, d);
+				draw_player((x * 50) + (SIZE / 2), (y * 50) + (SIZE / 2), PLAYER, d);
 			}
 			x++;
 		}
-		y++;
+	y++;
 	}
-	 while(i < minimap_height)
-	{
-		j = 0;
-		while(j < minimap_width)
-		{
-			mlx_pixel_put(d->mlx_ptr, d->win_ptr, j, i, 0xFF00);
-			j++;
-		}
-		i++;
-	} 
 }
 
- void	draw_wall()
+void draw_square(int x, int y, int color, t_data *d)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	while(i < minimap_height)
+	i = x;
+	while(i < x + SIZE)
 	{
-		j = 0;
-		while(j < minimap_width)
+		j = y;
+		while(j < y + SIZE)
 		{
-			mlx_pixel_put(d->mlx_ptr, d->win_ptr, j, i, 0xFF00);
+			mlx_pixel_put(d->mlx_ptr,d->win_ptr, i, j, color);
 			j++;
 		}
 		i++;
 	}
 }
 
-void	draw_floor()
+void draw_player(int x, int y, int color, t_data *d)
 {
+	int	i;
+	int	j;
+
+	i = x;
+	j = 1
+	while(j < x + 10)
+	{
+		mlx_pixel_put(d->mlx_ptr,d->win_ptr, i, y, color);
+		mlx_pixel_put(d->mlx_ptr,d->win_ptr, i + j, y, color);
+		mlx_pixel_put(d->mlx_ptr,d->win_ptr, i - j, y, color);
+		j++;
+	}
 
 }
-
-void	draw_player()
-{
-
-}  */
