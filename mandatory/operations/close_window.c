9144033d7fa386a9bcb8b9bf5c56cf9a5700bb8b
Rafael Soares <rafael@LAPTOP-L1QCHDIV>
2023-09-12 13:03:21 +0100

    x closing

--- mandatory/operations/close_window.c
+++ mandatory/operations/close_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   close_window.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jomirand <jomirand@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: emsoares <emsoares@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 12:34:50 by jomirand          #+#    #+#             */
-/*   Updated: 2023/09/12 12:44:26 by jomirand         ###   ########.fr       */
+/*   Updated: 2023/09/12 13:02:11 by emsoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int	ft_xbutton(t_data *d)
 {
-	mlx_destroy_window(d->mlx_ptr, d->win_ptr);
-	mlx_destroy_display(d->mlx_ptr);
-	free(d->mlx_ptr);
+	if (d != NULL && d->mlx_ptr != NULL && d->win_ptr != NULL) {
+        mlx_destroy_window(d->mlx_ptr, d->win_ptr);
+        mlx_destroy_display(d->mlx_ptr);
+        free(d->mlx_ptr);
+    }
 	exit(0);
 }
