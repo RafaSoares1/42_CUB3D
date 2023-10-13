c84301c4251800df3b513e1a6db24a84984f2ffe
Jose Pedro Miranda Goncalves <jomirand@c1r12s15.42porto.com>
2023-10-13 11:38:41 +0100

    erros

--- bonus/operations/check2_bonus.c
+++ bonus/operations/check2_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jomirand <jomirand@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:15:25 by emsoares          #+#    #+#             */
-/*   Updated: 2023/10/09 10:40:56 by jomirand         ###   ########.fr       */
+/*   Updated: 2023/10/13 11:35:39 by jomirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_check_textures(t_data *d)
 	i = ft_strlen(d->map_utils->no) - 1;
 	while (d->map_utils->no[i] != '.')
 		i--;
-	if (ft_strncmp((d->map_utils->no + i), ".xpm", 4) != 0)
+	if (!string_comp(d->map_utils->no + i, ".xpm"))
 	{
 		ft_free_game(d);
 		finish_error("Error: NO texture isn't .xpm!\n", 2);
@@ -27,7 +27,7 @@ void	ft_check_textures(t_data *d)
 	i = ft_strlen(d->map_utils->so) - 1;
 	while (d->map_utils->so[i] != '.')
 		i--;
-	if (ft_strncmp((d->map_utils->so + i), ".xpm", 4) != 0)
+	if (!string_comp(d->map_utils->so + i, ".xpm"))
 	{
 		ft_free_game(d);
 		finish_error("Error: SO texture isn't .xpm!\n", 2);
@@ -42,7 +42,7 @@ void	ft_check_textures2(t_data *d)
 	i = ft_strlen(d->map_utils->we) - 1;
 	while (d->map_utils->we[i] != '.')
 		i--;
-	if (ft_strncmp((d->map_utils->we + i), ".xpm", 4) != 0)
+	if (!string_comp(d->map_utils->we + i, ".xpm"))
 	{
 		ft_free_game(d);
 		finish_error("Error: WE texture isn't .xpm!\n", 2);
@@ -50,7 +50,7 @@ void	ft_check_textures2(t_data *d)
 	i = ft_strlen(d->map_utils->ea) - 1;
 	while (d->map_utils->ea[i] != '.')
 		i--;
-	if (ft_strncmp((d->map_utils->ea + i), ".xpm", 4) != 0)
+	if (!string_comp(d->map_utils->ea + i, ".xpm"))
 	{
 		ft_free_game(d);
 		finish_error("Error: EA texture isn't .xpm!\n", 2);
